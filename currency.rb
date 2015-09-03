6da494edcefc3137fbca1cb952ea3ea5e26fa5e2
Da-Me <damekim@DaMes-Mac.local>
2015-09-03 14:50:53 -0400

    created Currency Object with equal amounts

--- currency.rb
+++ currency.rb
@@ -1,2 +1,30 @@
-class Currency("USD")
-  @
+class Currency
+  def initialize(amount,code)
+  @amount = amount.to_i
+  @code = code.to_i
+  end
+end
+
+
+
+dollar = Currency.new(100.00, "USD")
+holladolla = Currency.new(100.00, "USD")
+
+
+
+
+=begin
+Currency objects:
+Should be created with an amount and a currency code
+Should equal another Currency object with the same amount and currency code
+Should NOT equal another Currency object with different amount or currency code
+Should be able to be added to another Currency object with the same currency code
+Should be able to be subtracted by another Currency object with the same currency code
+Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects
+with different currency codes.
+Should be able to be multiplied by a Fixnum or Float and return a Currency object
+Currency.new should be able to take one argument with a currency symbol embedded in it,
+like "$1.20" or "€ 7.00", and figure out the correct currency code. It can also take two
+arguments like before, one being the amount and the other being
+the currency code.
+=end
